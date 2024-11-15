a513494d3c73e08025d0b285d6ed1cd3f99c4475
SuinLee10 <143365454+SuinLee10@users.noreply.github.com>
2024-11-16 00:28:37 +0900

    update shortcathon file update

---
+++ shortkathon/ForRapo/src/main/java/com/example/forrapo/ForRapoApplication.java
@@ -0,0 +1,13 @@
+package com.example.forrapo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForRapoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForRapoApplication.class, args);
+    }
+
+}
---
+++ shortkathon/ForRapo/src/test/java/com/example/forrapo/ForRapoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forrapo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForRapoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
